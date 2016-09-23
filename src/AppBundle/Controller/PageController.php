ccb52f18453415f94e520a8a1fadba3e5ba03388
zoltran <zoltran86@gmail.com>
2016-09-23 17:03:08 +0200

    #18

---
+++ src/AppBundle/Controller/PageController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zoltran
+ * Date: 23.09.16
+ * Time: 16:46
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/oferta", name="ofertPage")
+     */
+    public function ofertActive()
+    {
+        return $this->render('page/ofert.html.twig');
+    }
+
+    /**
+     * @Route("/kontakt", name="contactPage")
+     */
+    public function contactActive()
+    {
+        return $this->render('page/contact.html.twig');
+    }
+
+    /**
+     * @Route("/onas", name="aboutPage")
+     */
+    public function aboutActive()
+    {
+        return $this->render('page/about.html.twig');
+    }
+}
