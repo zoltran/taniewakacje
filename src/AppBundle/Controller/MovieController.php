4d051bc3959d6988b901966f6a404572af041d91
Marek Siewiorek <marek.siewiorek@famaprom.pl>
2016-09-27 11:26:17 +0200

    #22 dodanie

--- src/AppBundle/Controller/MovieController.php
+++ src/AppBundle/Controller/MovieController.php
@@ -13,11 +13,22 @@ class MovieController extends Controller
     /**
      * @Route("/kontakt/form", name="formContact")
      */
-    public function indexAction()
+    public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
     {
         $movie= new Movie();
-        
+
         $form= $this->createForm(MovieType::class, $movie);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($movie);
+            $em->flush();
+
+            $this->addFlash('success', 'Udało się wysłać wiadomość');
+
+            return $this->redirectToRoute('formList', array());
+        }
 
         return $this->render('page/contact.html.twig', array(
             'form' => $form->createView()
@@ -29,6 +40,12 @@ class MovieController extends Controller
      */
     public function listAction()
     {
-        return new Response('test');
+        $em = $this->getDoctrine()->getManager();
+        $movies= $em->getRepository('AppBundle:Movie')
+            ->findAll();
+
+        return $this->render('movie/list.html.twig', array(
+            'movies' => $movies
+        ));
     }
 }
